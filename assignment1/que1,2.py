744a2426162d2e25478823207039a7baa0428192
1Manya <msingh12_be23@thapar.edu>
2025-01-15 19:55:17 +0530

    Add files via upload

---
+++ assignment1/que1,2.py
@@ -0,0 +1,16 @@
+#q1
+print("lets learn python")
+#q2- 2.1
+a=int(input("enter a"))
+b=int(input("enter b"))
+sum=a+b
+print("sum is",sum)
+#q2-2.2
+str1="manya"
+str2="singh"
+result= str1+str2
+print("concatenate string is:",result)
+#q2-2.3
+name = "MANYA"
+rollno= 102317119
+print(name+ "  "+str(rollno))
